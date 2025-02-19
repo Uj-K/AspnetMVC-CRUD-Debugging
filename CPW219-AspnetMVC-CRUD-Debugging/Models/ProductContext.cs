e5818412af595b26e225df78dc4b05745ff0b7e8
Uj-K <umkrall@gmail.com>
2025-02-18 19:16:16 -0800

    Create the database using code first migrations and a database context class

--- CPW219-AspnetMVC-CRUD-Debugging/Models/ProductContext.cs
+++ CPW219-AspnetMVC-CRUD-Debugging/Models/ProductContext.cs
@@ -6,8 +6,14 @@ namespace CPW219_AspnetMVC_CRUD_Debugging.Models
     {
         public ProductContext(DbContextOptions<ProductContext> options) : base(options)
         {
-
+            
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=cpw219Debugging;Trusted_Connection=True;");
+        }
+
         public DbSet<Product> Product { get; set; }
     }
 }
